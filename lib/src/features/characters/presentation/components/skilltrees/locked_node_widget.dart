cf58006f937b75ccc7a1b08744f86ecf94834b49
Jonas Curth <mail@jonascurth.de>
2022-11-26 23:52:27 +0100

    show the node tooltip for all nodes in the skilltree

--- lib/src/features/characters/presentation/components/skilltrees/locked_node_widget.dart
+++ lib/src/features/characters/presentation/components/skilltrees/locked_node_widget.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import '../../../../admin/skilltrees/domain/node.dart';
 
 import 'node_lock.dart';
+import 'node_tooltip.dart';
 
 class LockedNodeWidget extends StatefulWidget {
   final Node item;
@@ -21,34 +22,37 @@ class _LockedNodeWidgetState extends State<LockedNodeWidget> {
     return Positioned(
       left: widget.item.xPos,
       top: widget.item.yPos,
-      child: Transform.rotate(
-        angle: pi / 4,
-        child: Card(
-          clipBehavior: Clip.hardEdge,
-          color: Color(int.parse(widget.item.color)),
-          child: Stack(
-            children: [
-              Positioned.fill(child: Container(color: Colors.black54)),
-              Transform.rotate(
-                angle: -pi / 4,
-                child: Stack(
-                  children: [
-                    Positioned.fill(child: NodeLock(widget.item.cost)),
-                    Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: null != widget.item.skill.iconUrl
-                          ? Ink.image(
-                              image: NetworkImage(widget.item.skill.iconUrl!),
-                              fit: BoxFit.fill,
-                              width: 32,
-                              height: 32,
-                            )
-                          : const SizedBox(width: 32, height: 32),
-                    )
-                  ],
+      child: NodeTooltip(
+        widget.item,
+        child: Transform.rotate(
+          angle: pi / 4,
+          child: Card(
+            clipBehavior: Clip.hardEdge,
+            color: Color(int.parse(widget.item.color)),
+            child: Stack(
+              children: [
+                Positioned.fill(child: Container(color: Colors.black54)),
+                Transform.rotate(
+                  angle: -pi / 4,
+                  child: Stack(
+                    children: [
+                      Positioned.fill(child: NodeLock(widget.item.cost)),
+                      Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: null != widget.item.skill.iconUrl
+                            ? Ink.image(
+                                image: NetworkImage(widget.item.skill.iconUrl!),
+                                fit: BoxFit.fill,
+                                width: 32,
+                                height: 32,
+                              )
+                            : const SizedBox(width: 32, height: 32),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
