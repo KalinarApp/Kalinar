fbebc9772704b39c4f48b53c95302a2fe75fd5f9
Jonas Curth <30692921+joCur@users.noreply.github.com>
2022-12-01 16:58:40 +0100

    Mehr Tabs sind besser als wenige Tabs!

--- lib/src/features/characters/presentation/components/details/character_sheet_widget.dart
+++ lib/src/features/characters/presentation/components/details/character_sheet_widget.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:kalinar/src/features/characters/application/character_controller.dart';
-import 'package:kalinar/src/features/characters/presentation/components/details/auto_saving_text_field.dart';
-import 'package:kalinar/src/features/characters/presentation/components/details/character_attributes.dart';
 
 import '../../../domain/character.dart';
 
+import 'character_attributes.dart';
 import 'character_information.dart';
 
 class CharacterSheetWidget extends ConsumerStatefulWidget {
@@ -34,18 +30,6 @@ class _CharacterSheetWidgetState extends ConsumerState<CharacterSheetWidget> {
         Padding(
           padding: const EdgeInsets.all(4),
           child: CharacterAttributes(widget.character),
-        ),
-        AutoSavingTextField(
-          title: AppLocalizations.of(context)!.characterInventory,
-          initialValue: widget.character.inventory,
-          onSaving: (currentText) async {
-            await ref.read(characterControllerProvider).update(widget.character.id, {"inventory": currentText});
-          },
-        ),
-        AutoSavingTextField(
-          title: AppLocalizations.of(context)!.characterNotes,
-          initialValue: widget.character.notes,
-          onSaving: (currentText) async => await ref.read(characterControllerProvider).update(widget.character.id, {"notes": currentText}),
         ),
       ],
     );
