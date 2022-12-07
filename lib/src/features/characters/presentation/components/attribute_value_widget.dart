679544a25d43c34140e681b4088241667a2181f1
Jonas Curth <mail@jonascurth.de>
2022-12-07 22:30:00 +0100

    stretch attribute overview over the complete width

--- lib/src/features/characters/presentation/components/attribute_value_widget.dart
+++ lib/src/features/characters/presentation/components/attribute_value_widget.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_iconpicker/flutter_iconpicker.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:just_the_tooltip/just_the_tooltip.dart';
@@ -41,31 +40,28 @@ class AttributeValueWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final icon = null != value.attribute.iconData ? deserializeIcon(json.decode(value.attribute.iconData!)) : null;
 
-    return ConstrainedBox(
-      constraints: const BoxConstraints(maxWidth: 155),
-      child: showTitle
-          ? ListTile(
-              dense: true,
-              horizontalTitleGap: 0,
-              minVerticalPadding: 0,
-              minLeadingWidth: 28,
-              contentPadding: const EdgeInsets.all(0),
-              visualDensity: const VisualDensity(vertical: -4),
-              title: showTitle ? _tooltip(context, Text(value.attribute.name, maxLines: 2, overflow: TextOverflow.ellipsis)) : null,
-              trailing: _tooltip(context, Text(value.value.toString(), textAlign: TextAlign.right)),
-              leading: _tooltip(context, FaIcon(icon ?? FontAwesomeIcons.tag, size: 20)),
-            )
-          : _tooltip(
-              context,
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  FaIcon(icon ?? FontAwesomeIcons.tag, size: 20),
-                  const SizedBox(width: 4),
-                  Text(value.value.toString()),
-                ],
-              ),
+    return showTitle
+        ? ListTile(
+            dense: true,
+            horizontalTitleGap: 0,
+            minVerticalPadding: 0,
+            minLeadingWidth: 28,
+            contentPadding: const EdgeInsets.all(0),
+            visualDensity: const VisualDensity(vertical: -4),
+            title: showTitle ? _tooltip(context, Text(value.attribute.name, maxLines: 2, overflow: TextOverflow.ellipsis)) : null,
+            trailing: _tooltip(context, Text(value.value.toString(), textAlign: TextAlign.right)),
+            leading: _tooltip(context, FaIcon(icon ?? FontAwesomeIcons.tag, size: 20)),
+          )
+        : _tooltip(
+            context,
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FaIcon(icon ?? FontAwesomeIcons.tag, size: 20),
+                const SizedBox(width: 4),
+                Text(value.value.toString()),
+              ],
             ),
-    );
+          );
   }
 }
