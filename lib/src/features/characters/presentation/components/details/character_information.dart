64f4858a8eb536305df98d4b6a4fd7d884b8d5fb
Jonas Curth <mail@jonascurth.de>
2022-12-07 22:31:09 +0100

    set character information to fill rest of screen so the description can be multiple lines

--- lib/src/features/characters/presentation/components/details/character_information.dart
+++ lib/src/features/characters/presentation/components/details/character_information.dart
@@ -15,32 +15,33 @@ class CharacterInformation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisSize: MainAxisSize.max,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CharacterPortrait(item.iconUrl),
         const SizedBox(width: 10),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(item.name, style: Theme.of(context).textTheme.titleLarge),
-            if (null != item.description) Text(item.description!, style: Theme.of(context).textTheme.caption),
-            const SizedBox(height: 10),
-            SizedBox(
-              width: MediaQuery.of(context).size.width - 184,
-              child: Wrap(
-                spacing: 24,
-                runSpacing: 12,
-                children: [
-                  InformationRow(title: AppLocalizations.of(context)!.race, value: item.race.name),
-                  InformationRow(title: AppLocalizations.of(context)!.characterAge, value: item.age?.toString()),
-                  InformationRow(title: AppLocalizations.of(context)!.characterRelationship, value: item.relationship),
-                  InformationRow(title: AppLocalizations.of(context)!.characterProfession, value: item.profession),
-                  InformationRow(title: AppLocalizations.of(context)!.characterReligion, value: item.religion),
-                ],
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(item.name, style: Theme.of(context).textTheme.titleLarge),
+              if (null != item.description) Text(item.description!, style: Theme.of(context).textTheme.caption),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: MediaQuery.of(context).size.width - 184,
+                child: Wrap(
+                  spacing: 24,
+                  runSpacing: 12,
+                  children: [
+                    InformationRow(title: AppLocalizations.of(context)!.race, value: item.race.name),
+                    InformationRow(title: AppLocalizations.of(context)!.characterAge, value: item.age?.toString()),
+                    InformationRow(title: AppLocalizations.of(context)!.characterRelationship, value: item.relationship),
+                    InformationRow(title: AppLocalizations.of(context)!.characterProfession, value: item.profession),
+                    InformationRow(title: AppLocalizations.of(context)!.characterReligion, value: item.religion),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         )
       ],
     );
