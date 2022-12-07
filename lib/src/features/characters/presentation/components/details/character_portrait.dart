c0d8043a75f9caea72ef36668c37a7695849d20f
Jonas Curth <mail@jonascurth.de>
2022-12-07 22:30:29 +0100

    made the character portrait maximizable

--- lib/src/features/characters/presentation/components/details/character_portrait.dart
+++ lib/src/features/characters/presentation/components/details/character_portrait.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:animations/animations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class CharacterPortrait extends StatelessWidget {
@@ -9,12 +10,29 @@ class CharacterPortrait extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 150,
-      child: Card(
-        clipBehavior: Clip.hardEdge,
-        elevation: 4,
-        child: null != imageUrl ? Image.network(imageUrl!) : Center(child: Text(AppLocalizations.of(context)!.characterNoPortrait)),
+    final image = null == imageUrl ? null : Image.network(imageUrl!);
+    return OpenContainer(
+      closedElevation: 4,
+      closedColor: Theme.of(context).backgroundColor,
+      clipBehavior: Clip.hardEdge,
+      closedBuilder: (context, action) => SizedBox(
+        width: 150,
+        child: image ?? Center(child: Text(AppLocalizations.of(context)!.characterNoPortrait)),
+      ),
+      tappable: null != image,
+      openBuilder: (context, action) => Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Image.network(
+          imageUrl!,
+          fit: BoxFit.contain,
+          width: double.infinity,
+          height: double.infinity,
+          alignment: Alignment.center,
+        ),
       ),
     );
   }
