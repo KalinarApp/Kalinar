c435005cbb7fbd1d3ce7d56bd21126bc8eaf24e3
Jonas Curth <mail@jonascurth.de>
2023-03-20 22:37:26 +0100

    Neuladen des Charakters beim schließen der des Skilltrees

--- Client/lib/src/features/characters/presentation/components/details/character_skilltree_list.dart
+++ Client/lib/src/features/characters/presentation/components/details/character_skilltree_list.dart
@@ -1,35 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router_flow/go_router_flow.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../admin/skilltrees/domain/skilltree_overview.dart';
 import '../../../../admin/skilltrees/presentation/components/overview/skilltrees/skilltree_item.dart';
-import '../../../application/controllers/character_controller.dart';
 import '../../../domain/character.dart';
 import '../../skilltree_screen.dart';
 
-class CharacterSkilltreeList extends ConsumerStatefulWidget {
+class CharacterSkilltreeList extends ConsumerWidget {
   final Character character;
 
   const CharacterSkilltreeList(this.character, {super.key});
-
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _CharacterSkilltreeListState();
-}
-
-class _CharacterSkilltreeListState extends ConsumerState<CharacterSkilltreeList> {
-  _openSkilltree(SkilltreeOverview item) {
-    GoRouter.of(context)
-        .pushNamed(SkilltreeScreen.name, params: {"id": item.id}).then((value) => ref.read(characterControllerProvider).get(widget.character.id));
+  _openSkilltree(SkilltreeOverview item, BuildContext context) {
+    GoRouter.of(context).pushNamed(SkilltreeScreen.name, params: {"id": item.id});
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return ListView.builder(
-      itemCount: widget.character.skilltrees.length,
+      itemCount: character.skilltrees.length,
       itemBuilder: (context, index) => SkilltreeItem(
-        widget.character.skilltrees[index],
-        onPress: _openSkilltree,
+        character.skilltrees[index],
+        onPress: (item) => _openSkilltree(item, context),
       ),
     );
   }
