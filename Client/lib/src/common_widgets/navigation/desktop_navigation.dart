b72553e8e77f284b892729674c1c40ab2358e01f
Jonas Curth <mail@jonascurth.de>
2023-07-04 22:28:36 +0200

    Fusionierung der Seitenleisten

--- Client/lib/src/common_widgets/navigation/desktop_navigation.dart
+++ Client/lib/src/common_widgets/navigation/desktop_navigation.dart
@@ -1,48 +1,40 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_side_menu/flutter_side_menu.dart';
 import 'package:go_router/go_router.dart';
+import 'package:kalinar/src/features/characters/presentation/character_detail_screen.dart';
+import 'package:kalinar/src/features/characters/presentation/character_overview_screen.dart';
 
-import '../../features/characters/presentation/desktop/components/character_list.dart';
+import '../../features/characters/application/controllers/character_overview_controller.dart';
+import '../../features/characters/application/notifier/character_overview_state_notifier.dart';
 import '../user_menu.dart';
 import 'navigation_item.dart';
 
-class DesktopNavigation extends StatefulWidget {
+class DesktopNavigation extends ConsumerStatefulWidget {
   final Widget child;
   final List<NavigationItem> tabs;
 
   const DesktopNavigation({required this.child, required this.tabs, super.key});
 
   @override
-  State<DesktopNavigation> createState() => _DesktopNavigationState();
+  ConsumerState<DesktopNavigation> createState() => _DesktopNavigationState();
 }
 
-class _DesktopNavigationState extends State<DesktopNavigation> with TickerProviderStateMixin {
-  late final TabController controller;
-
-  String? _currentCharacterId;
-
-  int get _currentIndex => _locationToTabIndex(GoRouter.of(context).location);
-
-  int _locationToTabIndex(String location) {
-    final index = widget.tabs.indexWhere((t) => location.startsWith(t.route));
-    return index < 0 ? 0 : index;
-  }
-
-  void _onItemTapped(int tabIndex) {
-    if (tabIndex != _currentIndex) {
-      context.go(widget.tabs[tabIndex].route);
-    }
-  }
-
+class _DesktopNavigationState extends ConsumerState<DesktopNavigation> with TickerProviderStateMixin {
   @override
   void initState() {
-    controller = TabController(length: widget.tabs.length, vsync: this);
+    Future.delayed(Duration.zero, ref.read(charactersProvider).getAll);
 
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    final items = ref.watch(charactersStateProvider) ?? [];
+    items.sortBy((e) => e.name);
+
     return Scaffold(
       body: Row(
         children: [
@@ -52,27 +44,43 @@ class _DesktopNavigationState extends State<DesktopNavigation> with TickerProvid
             hasResizerToggle: false,
             builder: (data) {
               return SideMenuData(
-                  header: const Padding(
-                    padding: EdgeInsets.only(bottom: 20, top: 8),
-                    child: UserMenu(),
-                  ),
-                  items: widget.tabs
+                header: const SizedBox(height: 12),
+                items: [
+                  ...widget.tabs
                       .map(
                         (e) => SideMenuItemDataTile(
                           icon: Icon(e.icon),
                           title: e.title,
-                          isSelected: widget.tabs.indexOf(e) == _currentIndex,
-                          onTap: () {
-                            _onItemTapped(widget.tabs.indexOf(e));
-                            setState(() => _currentCharacterId = null);
-                          },
+                          isSelected: GoRouter.of(context).location == e.route,
+                          onTap: () => context.go(e.route),
                           hasSelectedLine: false,
                         ),
                       )
-                      .toList());
+                      .toList(),
+                  const SideMenuItemDataDivider(divider: Divider()),
+                  ...items.map(
+                    (e) => SideMenuItemDataTile(
+                      isSelected: GoRouter.of(context).location == "${CharacterOverviewScreen.route}/${e.id}",
+                      icon: null != e.iconUrl
+                          ? Padding(
+                              padding: data.isOpen ? const EdgeInsets.only(right: 8.0) : EdgeInsets.zero,
+                              child: CircleAvatar(backgroundImage: CachedNetworkImageProvider(e.iconUrl!)))
+                          : null,
+                      onTap: () => context.goNamed(CharacterDetailScreen.name, pathParameters: {"id": e.id}),
+                      hasSelectedLine: false,
+                    ),
+                  )
+                ],
+                footer: Column(
+                  children: const [
+                    UserMenu(),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              );
             },
           ),
-          CharacterList(selectedId: _currentCharacterId, onSelectionChanged: (id) => setState(() => _currentCharacterId = id)),
+          // CharacterList(selectedId: _currentCharacterId, onSelectionChanged: (id) => setState(() => _currentCharacterId = id)),
           Expanded(child: widget.child),
         ],
       ),
